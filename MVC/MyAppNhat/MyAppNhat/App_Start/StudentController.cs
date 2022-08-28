69b17ebd243e9ae645de1c0c44c7af8c30414dc2
vannhat123 <nhatnvd00481@gmail.com>
2022-08-28 15:00:01 +0700

    create project, create Controller Student and Index,ShowData

---
+++ MVC/MyAppNhat/MyAppNhat/App_Start/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAppNhat.App_Start
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["name"] = "Nguyen van nhat";
+            TempData["age"] = "28";
+            TempData["address"] = "Ha Noi";
+            return View("ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+
+   
+    }
+}
